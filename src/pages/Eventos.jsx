2a61343d486caa5a96fc9e09e740dfcc93dfcaeb
wdev-adrianabonfanti <adriana.bonfanti@ebix.com>
2025-05-16 00:43:12 -0300

    Deploy bosta

--- src/pages/Eventos.jsx
+++ src/pages/Eventos.jsx
@@ -53,7 +53,7 @@ export default function Eventos() {
 
   const aplicarFiltros = (evento) => {
     const ongOk =
-      filtros.ongsSelecionadas.length === 0 || filtros.ongsSelecionadas.includes(evento.ongId);
+      filtros.ongsSelecionadas.length === 0 || filtros.ongsSelecionadas.includes(evento.ong?._id);
     const cidadeOk =
       filtros.cidade.trim() === "" ||
       evento.cidade.toLowerCase().includes(filtros.cidade.trim().toLowerCase());
