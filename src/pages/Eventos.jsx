94e387b9f50536779fda5c1d660aee1261ce6d85
wdev-adrianabonfanti <adriana.bonfanti@ebix.com>
2025-05-15 21:52:49 -0300

    Deploy cadastro again

--- src/pages/Eventos.jsx
+++ src/pages/Eventos.jsx
@@ -193,7 +193,7 @@ export default function Eventos() {
                 >
                   Me lembrar
                 </a>
-                {evento.aceitaVoluntario && (
+                {evento.precisaVoluntario  && (
                   <button
                     onClick={() => setModalEvento(evento)}
                     className="bg-emerald-500 text-white px-4 py-1 rounded-full text-sm hover:bg-emerald-600"
