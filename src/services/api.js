d50534d3f2c7cd3859a1ed3cae850fb03b36c984
wdev-adrianabonfanti <adriana.bonfanti@ebix.com>
2025-05-19 17:56:04 -0300

    Deploy força3

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,8 @@
-const axios = require("axios");
+import axios from 'axios'; 
 
 const api = axios.create({
-  baseURL: process.env.VITE_API_URL
+  baseURL: import.meta.env.VITE_API_URL
+
 });
-
-console.log("URL da API:", process.env.VITE_API_URL);
-
-module.exports = api;
+console.log("URL da API:", import.meta.env.VITE_API_URL);
+export default api;
