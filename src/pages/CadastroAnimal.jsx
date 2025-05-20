6f177d69052abe6aea0ac9adc46aee5b930182b4
wdev-adrianabonfanti <adriana.bonfanti@ebix.com>
2025-05-20 15:07:44 -0300

    Deploy cadastro x

--- src/pages/CadastroAnimal.jsx
+++ src/pages/CadastroAnimal.jsx
@@ -153,7 +153,8 @@ if (user && (user._id || user.id)) {
       
       alert("Animal atualizado com sucesso!");
 } else {
-  const response = await axios.post(`${import.meta.env.VITE_API_URL}/animals`, data, {
+  const response = await axios.post(`${import.meta.env.VITE_API_URL}/ongs/animais`, data, {
+
     headers: {
       Authorization: `Bearer ${token}`,
     },
